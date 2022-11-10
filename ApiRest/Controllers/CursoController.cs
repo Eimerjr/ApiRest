366685254ec759a7930bea65307019a70db79f60
Eimerjr <pereaeimer@gmail.com>
2022-11-09 22:44:34 -0500

    Agregar archivos de proyecto.

---
+++ ApiRest/Controllers/CursoController.cs
@@ -0,0 +1,62 @@
+﻿using ApiRest.Models;
+using ApiRest.RepositorioDatos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiRest.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            RpCursos RpCurso = new RpCursos();
+            return Ok(RpCurso.getCursos());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            RpCursos RpCurso = new RpCursos();
+
+            var cursort = RpCurso.getCurso(id);
+
+            if (cursort == null)
+            {
+                var nf = NotFound("Curso " + id.ToString() + " no existe.");
+                return nf;
+            }
+
+            return Ok(cursort);
+        }
+
+        [HttpPost("agregar")]
+        public IActionResult AddCurso(CursoModel nuevoCurso)
+        {
+            RpCursos RpCurso = new RpCursos();
+            RpCurso.Agregar(nuevoCurso);
+            return CreatedAtAction(nameof(AddCurso), nuevoCurso);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult deleteCurso(int id )
+        {
+            RpCursos RpCurso = new RpCursos();
+
+            var cursort = RpCurso.getCurso(id);
+
+            if (cursort == null)
+            {
+                var nf = NotFound("Curso " + id.ToString() + " no existe, no se puede eliminar.");
+                return nf;
+            }
+
+            RpCurso.eliminar(cursort);
+
+            return Ok(RpCurso.getCursos());
+            
+        }
+    }
+}
---
+++ ApiRest/RepositorioDatos/RpCursos.cs
@@ -0,0 +1,38 @@
+﻿using ApiRest.Models;
+
+namespace ApiRest.RepositorioDatos
+{
+    public class RpCursos
+    {
+        public static List<CursoModel> listaCursos = new List<CursoModel>()
+        {
+            new CursoModel() { Id = 1, Name = "Matematicas" , numberStudents = 10 },
+            new CursoModel() { Id = 2, Name = "Ingenieria de software" , numberStudents = 12 },
+            new CursoModel() { Id = 3, Name = "Calculo diferencial" , numberStudents = 29 }
+        };
+
+        public IEnumerable<CursoModel> getCursos()
+        {
+            return listaCursos;
+        }
+
+        public CursoModel getCurso(int id)
+        {
+            var Cursos = listaCursos.Where(cur => cur.Id == id);
+
+            return Cursos.FirstOrDefault();
+        }
+
+        public void Agregar(CursoModel nuevoCurso)
+        {
+            listaCursos.Add(nuevoCurso);
+        }
+
+        public void eliminar(CursoModel nuevoCurso)
+        {
+            listaCursos.Remove(nuevoCurso);
+        }
+
+
+    }
+}
